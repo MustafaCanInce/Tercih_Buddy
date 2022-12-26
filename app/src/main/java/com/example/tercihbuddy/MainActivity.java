6e416c4b94c68729b2c075c42fd7238b6323fbfe
Mustafa Can İnce <canince66@gmail.com>
2022-12-27 00:44:15 +0300

    image downloader thread imp.

--- app/src/main/java/com/example/tercihbuddy/MainActivity.java
+++ app/src/main/java/com/example/tercihbuddy/MainActivity.java
@@ -12,9 +12,11 @@ import android.widget.Button;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -72,32 +74,53 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        // Create a new thread
-        Thread thread = new Thread(new Runnable() {
+        Thread imageThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    URL url = new URL("https://mk.edu.tr/");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    URL imageUrl = new URL("https://upload.wikimedia.org/wikipedia/tr/3/3c/Android-Marshmallow-logo-.jpg");
+                    HttpURLConnection connection = (HttpURLConnection) imageUrl.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setRequestProperty("User-Agent", "Mozilla/5.0");
                     int responseCode = connection.getResponseCode();
                     if (responseCode == 200) {
-                        System.out.println("Website working properly.");
+                        // Get the size of the image
+                        int contentLength = connection.getContentLength();
+                        // read the image data
+                        InputStream inputStream = connection.getInputStream();
+                        // write the image data to a byte array
+                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        int totalBytesRead = 0;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                            totalBytesRead += bytesRead;
+                        }
+                        // Close the input and output streams
+                        inputStream.close();
+                        outputStream.close();
+                        // Get the byte array
+                        byte[] imageData = outputStream.toByteArray();
+                        // Check if the image is the same size as the original
+                        if (totalBytesRead == contentLength) {
+                            System.out.println("Image downloaded successfully.");
+                        } else {
+                            System.out.println("Error downloading image: size does not match.");
+                        }
                     } else {
-                        System.out.println("Website is not working properly!!");
+                        System.out.println("Error downloading image: HTTP response code " + responseCode);
                     }
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
             }
         });
 
-        // Start the thread
-        thread.start();
+        imageThread.start();
+
+
 
     }
 
--- app/src/test/java/com/example/tercihbuddy/ExampleUnitTest.java
+++ app/src/test/java/com/example/tercihbuddy/ExampleUnitTest.java
@@ -10,15 +10,29 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    /**
+     * Example local unit test, which will execute on the development machine (host).
+     *
+     * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+     */
+
     User nu=new User();
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void mail_isCorrect() {
+        //Basic Mail check
+        nu.setEmail("must1@gmail.com");
+        assertEquals(nu.getEmail(),"must1@gmail.com");
     }
-
     @Test
     public void getIsActive_tb() {
+        //Basic Username check
         nu.setUsername("MUSTAFA");
         assertEquals(nu.getUsername(),"Mustafa");
+    }
+    @Test
+    public void pid_isCorrect() {
+        //Basic Pid check
+        nu.setPid("654321");
+        assertEquals(nu.getPid(),"654321");
     }
 }
